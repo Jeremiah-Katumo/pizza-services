1d026980c9e5d2022ee8114d77e7c669fa0d01a1
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-05-04 12:46:32 +0300

    updated main, schemas, models

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
-from routes.auth import auth_router
-from routes.orders import order_router
+from models import auth_models
+from routes.auth_routes import auth_router
+from routes.orders_routes import order_router
+from .database import engine
 
 app = FastAPI()
 
-@app.route("/")
+auth_models.Base.metadata.create_all(bind=engine)
+
+@app.get("/")
 def home():
-    return {"message":"Welcome Home"}
+    return {"message": "Welcome Home"}
 
 app.include_router(auth_router)
 app.include_router(order_router)
